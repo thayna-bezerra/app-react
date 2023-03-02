07f6a8ed2e175c8b01ceb4ed60f3c8c07db14387
thayna <arraisthayna2106@gmail.com>
2023-03-02 10:22:02 -0300

    componente post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,6 @@
-import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-
-import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
 import styles from "./App.module.css";
 import "./global.css";
