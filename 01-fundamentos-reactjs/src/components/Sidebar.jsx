b579df4d48ed93df97424ab7742ac44097d87bea
thayna <arraisthayna2106@gmail.com>
2023-03-03 19:10:45 -0300

    componente avatar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine} from 'phosphor-react'
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css';
 
 export function Sidebar(){
@@ -9,8 +10,7 @@ export function Sidebar(){
         src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/thayna-bezerra.png" />
-
+        <Avatar src="https://github.com/thayna-bezerra.png"/>
         <strong>Thayna Bezerra</strong>
         <span>Web Developer</span>
       </div>
